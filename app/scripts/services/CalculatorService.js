2bbcea7f963a3c001b0194defb120a42ef85ab74
beanrider <jweng2014@hotmail.com>
2017-11-02 16:20:54 -0400

    SD307-11 Added enum init, eligibility function, and validation for options and group number.

--- app/scripts/services/CalculatorService.js
+++ app/scripts/services/CalculatorService.js
@@ -4,11 +4,20 @@
     angular
         .module("testApp")
         .factory("CalculatorService", function () {
-
+            initEnums();
             return {
                 "getAnnualPension": getAnnualPension
             };
 
+            function initEnums() {
+                RetirementOption.A = new RetirementOption("A");
+                RetirementOption.B = new RetirementOption("B");
+                RetirementOption.C = new RetirementOption("C");
+
+                EmployeeGroup.ONE = new EmployeeGroup("1");
+                EmployeeGroup.TWO = new EmployeeGroup("2");
+                EmployeeGroup.FOUR = new EmployeeGroup("4");
+            }
 
             // Age factor * Years of Creditable Service * Highest 5 year average of annual pay = Option A annual allowance
             //
@@ -73,12 +82,26 @@
             }
 
             /**
+             * Represents an enumeration for all retirement plan options.
+             */
+            class EmployeeGroup {
+                constructor(name) {
+                    this.name = name;
+                }
+
+                toString() {
+                    return `EmployeeGroup.${this.name}`;
+                }
+            }
+
+
+            /**
              * Used for calculating pension for Option B
              * @param maxPension
              * @param retireAge_Years
              * @returns {number}
              */
-            function calculateForOptionB(maxPension, retireAge_Years) {
+            function calcOptionB(maxPension, retireAge_Years) {
                 let percentOff;
                 // TODO ask if this logic is what the client means.
                 if (0 <= retireAge_Years && retireAge_Years < 50) {
@@ -94,54 +117,116 @@
             }
 
             /**
+             * Based on the starting date of employment, retirement age, years worked, and option, determine if
+             * you are eligible for retirement.
+             *
+             * @param dateOfStartEmployment Date object, date of start employment
+             * @param retirementAge_Years   number, age of retirement in years
+             * @param yearsWorked           number, number of years worked
+             * @param employeeGroup         EmployeeGroup, what type of employee are you considered as
+             * @returns {boolean}
+             */
+            function isEligibleForRetirement(dateOfStartEmployment, retirementAge_Years, yearsWorked, employeeGroup) {
+                let secondPolicyDate = new Date("4/2/2012");
+                let isFirstPolicy = secondPolicyDate < dateOfStartEmployment;
+                if (isFirstPolicy) {
+                    if (yearsWorked >= 20) {
+                        return true;
+                    } else {
+                        return yearsWorked >= 10 && retirementAge_Years >= 55;
+                    }
+                } else {
+                    if (yearsWorked < 10) {
+                        return false
+                    }
+                    switch (employeeGroup) {
+                        case EmployeeGroup.ONE:
+                            return retirementAge_Years >= 60;
+                        case EmployeeGroup.TWO:
+                            return retirementAge_Years >= 55;
+                        case EmployeeGroup.FOUR:
+                            return retirementAge_Years >= 50;
+                        default:
+                            throw Error("Invalid EmployeeGroup option: " + employeeGroup);
+                    }
+                }
+            }
+
+            function convertOptionToEnum(option) {
+                switch (option) {
+                    case RetirementOption.A.name:
+                        return RetirementOption.A;
+                    case RetirementOption.B.name:
+                        return RetirementOption.B;
+                    case RetirementOption.C.name:
+                        return RetirementOption.C;
+                    default:
+                        throw new Error("Unknown retirement option: " + option);
+                }
+            }
+
+            function convertGroupToEnum(group) {
+                switch (group) {
+                    case EmployeeGroup.ONE.name:
+                        return EmployeeGroup.ONE;
+                    case EmployeeGroup.TWO.name:
+                        return EmployeeGroup.TWO;
+                    case EmployeeGroup.FOUR.name:
+                        return EmployeeGroup.FOUR;
+                    default:
+                        throw new Error("Unknown employee group: " + group)
+                }
+            }
+
+            /**
              * Exposed function for calculating the annual pension.
              *
              * @param highestAverageSalary Float, highest average salary in dollars
-             * @param yearsWorked Integer, years worked before retirement
-             * @param dateOfBirth Date object, birth date
-             * @param dateOfRetirement Date object, retirement date
-             * @param group String, this employee's group
-             * @param isVeteran boolean, is employee a veteran?
-             * @param hasBene boolean, do you have a benefactor?
-             * @param beneDoB Date object, benefactor birthdate.
-             * @param option String, what type of retirement plan
+             * @param yearsWorked          Integer, years worked before retirement
+             * @param dateOfBirth          Date object, birth date
+             * @param dateOfStartEmployment Date object, date of start employment
+             * @param dateOfRetirement     Date object, retirement date
+             * @param groupNum             String, this employee's group number
+             * @param isVeteran            boolean, is employee a veteran?
+             * @param hasBene              boolean, do you have a benefactor?
+             * @param beneDoB              Date object, benefactor birthdate.
+             * @param retireOption         String, what type of retirement plan
              * @returns {*}
              */
             function getAnnualPension(highestAverageSalary, yearsWorked,
-                                      dateOfBirth, dateOfRetirement,
-                                      group, isVeteran,
+                                      dateOfBirth, dateOfStartEmployment, dateOfRetirement,
+                                      groupNum, isVeteran,
                                       hasBene, beneDoB,
-                                      option) {
-
-                RetirementOption.A = new RetirementOption("A");
-                RetirementOption.B = new RetirementOption("B");
-                RetirementOption.C = new RetirementOption("C");
+                                      retireOption) {
 
                 // TODO more validation.
+
+                const optionEnum = convertOptionToEnum(retireOption);
+                const groupEnum = convertGroupToEnum(groupNum);
                 const retireAge_Years = Math.round(
                     (dateOfRetirement.getTime() - dateOfBirth.getTime()) / (1000 * 3600 * 24 * 365));
 
-                // Check if retirement age is okay
-                if (retireAge_Years < 36) {
-                    return 0;
-                }
-
-                // Check if years worked is okay
-                if (yearsWorked <= 10) {
-                    return 0;
+                // Eligible for retirement
+                if (isEligibleForRetirement(dateOfStartEmployment, retireAge_Years, yearsWorked, groupEnum)) {
+                    throw "Not eligible for retirement!";
                 }
 
                 const maxAnnualPension = getMaxAnnualPension(highestAverageSalary, yearsWorked,
-                                                                dateOfBirth, dateOfRetirement, group, isVeteran);
+                                                                dateOfBirth, dateOfRetirement, groupNum, isVeteran);
                 
-                switch (option) {
-                    case RetirementOption.A.name:
+                switch (optionEnum) {
+                    case RetirementOption.A:
                         return {"annualPension": maxAnnualPension, "beneAnnualPension": 0};
-                    case RetirementOption.B.name:
-                        return {"annualPension": calculateForOptionB(maxAnnualPension, retireAge_Years), "beneAnnualPension": 0};
-                    case RetirementOption.C.name:
-                        // TODO Implement this: need to clarify calculation method.
-                        return {"annualPension": 0, "beneAnnualPension": 0};
+                    case RetirementOption.B:
+                        return {"annualPension": calcOptionB(maxAnnualPension, retireAge_Years),
+                                "beneAnnualPension": 0};
+                    case RetirementOption.C:
+                        if (!hasBene) {
+                            throw "Option C requires a benefactor!"
+                        } else {
+                            // TODO Implement this: need to clarify calculation method.
+                        }
+                        throw "Option C is not yet implemented!";
                 }
             }
         });
