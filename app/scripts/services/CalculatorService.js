2871de50d88f62236b37ce167770efc49c711814
beanrider <jweng2014@hotmail.com>
2017-11-02 01:48:57 -0400

    Added logic to getAnnualPension

--- app/scripts/services/CalculatorService.js
+++ app/scripts/services/CalculatorService.js
@@ -64,8 +64,78 @@
                 return getAgeFactor(dateOfBirth, dateOfRetirement, group) ;
             }
             
-            function getAnnualPension(maxAnnualPension, option) {
+            /**
+             * Represents an enumeration for all retirement plan options.
+             */
+            class RetirementOption {
+                constructor(name) {
+                    this.name = name;
+                }
+
+                toString() {
+                    return `RetirementOption.${this.name}`;
+                }
+            }
+
+            RetirementOption.A = RetirementOption("A");
+            RetirementOption.B = RetirementOption("B");
+            RetirementOption.C = RetirementOption("C");
+
+            function calculateForOptionB(maxPension, retireAge_Years) {
+                let percentOff;
+                // TODO ask if this logic is what the client means.
+                if (0 <= retireAge_Years && retireAge_Years < 50) {
+                    percentOff = 0;
+                } else if (50 <= retireAge_Years && retireAge_Years < 60) {
+                    percentOff = 0.01;
+                } else if (60 <= ageYear && ageYear < 70) {
+                    percentOff = 0.03;
+                } else {
+                    percentOff = 0.05;
+                }
+                return maxPension * (1.0 - percentOff);
+            }
+
+            /**
+             * Exposed function for calculating the annual pension.
+             * 
+             * highestAverageSalary: Float, highest average salary in dollars
+             * yearsWorked: Integer, years worked before retirement
+             * dateOfBirth: Date object, birth date
+             * dateOfRetirement: Date object, retirement date
+             * group: String, this employee's group
+             * isVeteran: boolean, is employee a veteran?
+             * 
+             * hasBene: boolean, do you have a benefactor?
+             * beneDoB: Date object, benefactor birthdate.
+             *
+             * option: RetirementOption object, what type of retirement plan
+             */
+            function getAnnualPension(highestAverageSalary, yearsWorked, dateOfBirth, dateOfRetirement, group, isVeteran,
+                                        hasBene, beneDoB,
+                                        option) {
+                // TODO more validation.
+                // TODO Check if age is less than 36, if so throw error.
                 
+                
+                // Check if years is less than 10, if so return 0;
+                if (yearsWorked === 0) {
+                    return 0;
+                }
+
+                const maxAnnualPension = getMaxAnnualPension(highestAverageSalary, yearsWorked, dateOfBirth, dateOfRetirement, group, isVeteran);
+                
+                witch (option) {
+                    case RetirementOption.A:
+                        return maxAnnualPension;
+                    case RetirementOption.B:
+                        // TODO calculate year component in age
+                        let retireAge_Years = dateOfRetirement - dateOfBirth;
+                        return calculateForOptionB(maxAnnualPension, retireAge_Years);
+                    case RetirementOption.C:
+                        // TODO clarify algorithm.
+                        return;
+                }
             }
 
         });
