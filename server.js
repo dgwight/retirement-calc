d417cc30b2385d696315aa785a5cd4a80f06e583
DJ Strasser <djstras963@gmail.com>
2017-11-09 14:05:15 -0500

    Ehh this branch might be dead

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const express = require('express')
 const app = express()
 
+// Expose Angular app and public files
 app.use(express.static(__dirname + '/public'));
+
+// Run the rest of the node app
+require("./routes/index.js")
+
 app.listen(process.env.PORT || 3000);
 console.log('PENSION MASTER listening on port 3000!')
