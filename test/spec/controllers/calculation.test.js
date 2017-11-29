79de0c4ae23b48563ae1061d7d4fe80cdb3405b8
wengjef <wengjef@ccs.neu.edu>
2017-11-29 15:56:07 -0500

    SD307-42 Fixed tests to work with the new calc service.

--- test/spec/controllers/calculation.test.js
+++ test/spec/controllers/calculation.test.js
@@ -23,80 +23,82 @@ describe('Test CalculatorService: ', function () {
     expect(CalculatorService.getAnnualPension(
       30000,
       moment("January 1, 1920", "MMMM D, YYYY"),  moment("January 1, 1930", "MMMM D, YYYY"), moment("January 1, 2018", "MMMM D, YYYY"),
-      "1", null,
+      "1", 10, false,
       moment("January 1, 2015", "MMMM D, YYYY"),
       "A"))
-      .toEqual({annualPension: 66000})});
+      .toEqual({annualPension: 7650})});
 
   it("Option A Group 2", function() {
     expect(CalculatorService.getAnnualPension(
       50000,
       moment("March 14, 1940", "MMMM DD, YYYY"),  moment("April 4, 1960", "MMMM D, YYYY"), moment("February 17, 2010", "MMMM DD, YYYY"),
-      "2", null,
+      "2", 20, false,
       moment("January 1, 2015", "MMMM D, YYYY"),
       "A"))
-      .toEqual({annualPension: 62500})});
+      .toEqual({annualPension: 25300})});
 
   it("Option A Group 4", function() {
     expect(CalculatorService.getAnnualPension(
       50000,
       moment("May 22, 1950", "MMMM DD, YYYY"),  moment("June 4, 1970", "MMMM D, YYYY"), moment("July 17, 2010", "MMMM DD, YYYY"),
-      "4", null,
+      "4", 20, false,
       moment("January 1, 2015", "MMMM D, YYYY"),
       "A"))
-      .toEqual({annualPension: 50000})});
+      .toEqual({annualPension: 25300})});
 
   it("Option B Group 1", function() {
     expect(CalculatorService.getAnnualPension(
       30000,
       moment("January 1, 1920", "MMMM D, YYYY"),  moment("January 1, 1930", "MMMM D, YYYY"), moment("January 1, 2018", "MMMM D, YYYY"),
-      "1", null,
+      "1", 10, false,
       moment("January 1, 2015", "MMMM D, YYYY"),
       "B"))
-      .toEqual({annualPension: 62700})});
+      .toEqual({annualPension: 7267.5})});
 
   it("Option B Group 2", function() {
     expect(CalculatorService.getAnnualPension(
       50000,
       moment("March 14, 1940", "MMMM DD, YYYY"),  moment("April 4, 1960", "MMMM D, YYYY"), moment("February 17, 2010", "MMMM DD, YYYY"),
-      "1", null,
+      "1", 20, false,
       moment("January 1, 2015", "MMMM D, YYYY"),
       "B"))
-      .toEqual({annualPension: 59375})});
+      .toEqual({annualPension: 24035})});
 
   it("Option B Group 4", function() {
     expect(CalculatorService.getAnnualPension(
       50000,
       moment("May 22, 1950", "MMMM DD, YYYY"),  moment("June 4, 1970", "MMMM D, YYYY"), moment("July 17, 2010", "MMMM DD, YYYY"),
-      "4", null,
+      "4", 20, false,
       moment("January 1, 2015", "MMMM D, YYYY"),
       "B"))
-      .toEqual({annualPension: 48500})});
+      .toEqual({annualPension: 24541})});
 
   it("Option C Group 1", function() {
     expect(CalculatorService.getAnnualPension(
       30000,
       moment("January 1, 1920", "MMMM D, YYYY"),  moment("January 1, 1930", "MMMM D, YYYY"), moment("January 1, 2018", "MMMM D, YYYY"),
-      "1", null,
+      "1", 10, false,
       moment("January 1, 2005", "MMMM D, YYYY"),
       "C"))
-      .toEqual({annualPension: 62040})});
+      .toEqual({annualPension: 7191})});
 
   it("Option C Group 2", function() {
     expect(CalculatorService.getAnnualPension(
       50000,
       moment("March 14, 1940", "MMMM DD, YYYY"),  moment("April 4, 1960", "MMMM D, YYYY"), moment("February 17, 2010", "MMMM DD, YYYY"),
-      "1", null,
+      "1", 20, false,
       moment("January 1, 2010", "MMMM D, YYYY"),
       "C"))
-      .toEqual({annualPension: 58750})});
+      .toEqual({annualPension: 23782})});
 
   it("Option C Group 4", function() {
     expect(CalculatorService.getAnnualPension(
       50000,
       moment("May 22, 1950", "MMMM DD, YYYY"),  moment("June 4, 1970", "MMMM D, YYYY"), moment("July 17, 2010", "MMMM DD, YYYY"),
-      "4", null,
+      "4", 20, false,
       moment("January 1, 2015", "MMMM D, YYYY"),
       "C"))
-      .toEqual({annualPension: 43000})});
+      .toEqual({annualPension: 21758})});
+
+  // TODO veteran tests
 });
